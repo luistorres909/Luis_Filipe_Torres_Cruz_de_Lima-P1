f1bb917b9a34d99df61fcbeaca5d47181b76a52d
luistorres909 <lipinhowinho@hotmail.com>
2019-10-02 00:34:26 -0300

    Questão 3 Corrigida

--- Q3.py
+++ Q3.py
@@ -1,4 +1,25 @@
+Durante a prova me esqueci se existia algum operador para módulo, e pensei em
+fazer esta função, porém fiquei receoso de colocar mais uma função no programa.
+Segue a função que retira o valor absoluto de um número. Além disso, ao realizar
+a questão considerei que deveria exibir todos os valor de pi(n) conforme n aumenta
+e caminha para o caso em que sua diferença seja a requisitada na questão, assim
+podemos ver que pi(n) se aproxima de pi conforme n cresce !
+
+
+
+def mod(n):
+    if (n >= 0):
+
+        return n
+    
+    if (n < 0) :
+
+        return -n    
+
+
+
 def pi(n):
+
 
     resultado=0
     valor=0
@@ -17,7 +38,7 @@ def main() :
 
     n = 1
 
-    while (módulo(pi(n)-pi(n-1)) > 5*10**(-8)) :
+    while (mod(pi(n)-pi(n-1)) > 5*10**(-8)) :
 
         print(pi(n))
 
